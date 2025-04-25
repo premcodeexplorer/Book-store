d336e6a5e392ad96169736935b5c17c28bbfd106
Baba Prem Prakash <152669128+premcodeexplorer@users.noreply.github.com>
2025-04-25 20:57:34 +0530

    Update settings.py

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -49,30 +49,21 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'bookstore.wsgi.application'
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'bookstore_db',
-#         'USER': 'root',
-#         'PASSWORD': 'Prembr@23',  
-#         'HOST': 'localhost',
-#         'PORT': '3306',
-#         'OPTIONS': {
-#             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-#             'charset': 'utf8mb4',
-#         }
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('POSTGRES_DB', 'bookstore'),
-        'USER': os.getenv('POSTGRES_USER', 'bookstore'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'bookstore'),
-        'HOST': os.getenv('POSTGRES_HOST', 'db'),
-        'PORT': os.getenv('POSTGRES_PORT', '5432'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'bookstore_db',
+        'USER': 'root',
+        'PASSWORD': 'Prembr@23',  
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4',
+        }
     }
 }
+
 
 STATIC_ROOT = '/staticfiles'
 
